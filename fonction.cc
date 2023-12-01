05f800fe147a677c6d4f989b6df5a5af35f03921
amin-hdn <94766481+amin-hdn@users.noreply.github.com>
2023-12-01 02:44:12 +0100

    Update fonction.cc

--- fonction.cc
+++ fonction.cc
@@ -9,3 +9,23 @@ void PrintNodeIPAddresses(Ptr<Node> node, std::string nodeName)
         std::cout << nodeName << " => node " << node->GetId() << " ==> IP address: " << ipAddress << std::endl;
     }
 }
+
+    // Désactiver toutes les interfaces du noeud
+
+    Ptr<Ipv4> ipv4 = node->GetObject<Ipv4>();
+
+    for (uint32_t i = 0; i < ipv4->GetNInterfaces(); i++) {
+
+        ipv4->SetDown(i);
+
+    }
+
+    NS_LOG_DEBUG ("A t=2s, noeud en cours de désactivation ! ");
+
+    Ipv4GlobalRoutingHelper::RecomputeRoutingTables();
+
+    //
+
+
+
+}Simulator::Schedule(Seconds(2.5), &TurnOffNode, csmaNodes2.Get(0) // node libaghi ta9tal);
