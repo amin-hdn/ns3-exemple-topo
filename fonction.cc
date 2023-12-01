3a753c470fd818e8cfe1a1d38e40310212a39d1b
amin-hdn <amine_hadnane@um5.ac.ma>
2023-12-01 03:43:24 +0100

    least but not last problem

--- fonction.cc
+++ fonction.cc
@@ -1,39 +1,5 @@
-<<<<<<< HEAD
-// Function to print IP addresses of a node
-void PrintNodeIPAddresses(Ptr<Node> node, std::string nodeName)
-{
-    Ptr<Ipv4> ipv4 = node->GetObject<Ipv4>();
-    for (uint32_t i = 1; i < ipv4->GetNInterfaces(); ++i)
-    {
-        Ipv4InterfaceAddress ifaceAddress = ipv4->GetAddress(i, 0);
-        Ipv4Address ipAddress = ifaceAddress.GetLocal();
-        std::cout << nodeName << " => node " << node->GetId() << " ==> IP address: " << ipAddress << std::endl;
-    }
-}
-=======
-void TurnOffNode(Ptr<Node> node) {
->>>>>>> 90a97b1da3f946f2fb2e3081ed88b4cf87d23a04
 
-    // Désactiver toutes les interfaces du noeud
-
-    Ptr<Ipv4> ipv4 = node->GetObject<Ipv4>();
-
-    for (uint32_t i = 0; i < ipv4->GetNInterfaces(); i++) {
-
-        ipv4->SetDown(i);
-
-    }
-
-    NS_LOG_DEBUG ("A t=2s, noeud en cours de désactivation ! ");
-
-    Ipv4GlobalRoutingHelper::RecomputeRoutingTables();
-
-    //
-
-
-
-<<<<<<< HEAD
-}Simulator::Schedule(Seconds(2.5), &TurnOffNode, csmaNodes2.Get(0) // node libaghi ta9tal);
+Simulator::Schedule(Seconds(2.5), &TurnOffNode, csmaNodes2.Get(0) // node libaghi ta9tal);
 =======
 }
 >>>>>>> 90a97b1da3f946f2fb2e3081ed88b4cf87d23a04
